77a2c30db0b5ffa5e12d4606b7d27421aa37717b
Franck Théolade <frtheola@microsoft.com>
2019-09-09 15:31:12 +0100

    polished up plumbing instruction and after code

---
+++ Lab#2 - Building a DSL/eShopWebFunctionalTests/Configuration/FunctionalUITest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopWebFunctionalTests.Configuration
+{
+    [TestClass]
+    public abstract class FunctionalUITest
+    {
+        private readonly Func<BrowserHost> _browserHostFactory;
+        protected BrowserHost Browser { get; private set; }
+
+        public FunctionalUITest(Func<BrowserHost> browserHostFactory)
+        {
+            _browserHostFactory = browserHostFactory;
+        }
+
+        [TestInitialize]
+        public virtual void RunBeforeEachTests()
+        {
+            Browser = _browserHostFactory();
+        }
+
+
+        [TestCleanup]
+        public virtual void RunAfterEachTests()
+        {
+            if (Browser != null)
+            {
+                Browser.Dispose();
+                Browser = null;
+            }
+        }
+    }
+}
---
+++ Lab#2 - Building a DSL/eShopWebFunctionalTests/Step 1/AddingToBasketAndCheckingOutTests .cs
@@ -0,0 +1,13 @@
+﻿using eShopWebFunctionalTests.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eShopWebFunctionalTests.Step_1
+{
+    [TestClass]
+    public class AddingToBasketAndCheckingOutTests : FunctionalUITest
+    {
+        public AddingToBasketAndCheckingOutTests() : base(BrowserHost.Chrome)  { }
+
+
+    }
+}
