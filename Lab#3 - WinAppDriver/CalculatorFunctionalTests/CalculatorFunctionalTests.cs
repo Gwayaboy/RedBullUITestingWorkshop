fab5e4c4d4be80d62cca0ec579ee83250f506509
Franck Théolade <frtheola@microsoft.com>
2019-09-12 01:13:49 +0100

    starting point for lab

--- Lab#3 - WinAppDriver/CalculatorFunctionalTests/CalculatorFunctionalTests.cs
+++ Lab#3 - WinAppDriver/CalculatorFunctionalTests/CalculatorFunctionalTests.cs
@@ -9,32 +9,6 @@ namespace CalculatorFunctionalTests
     [TestClass]
     public class CalculatorFunctionalTests : FunctionalTests
     {
-        public CalculatorWindow Calculator { get; private set; }
-
-
-        [TestInitialize]
-        public void Setup()
-        {
-            Calculator = WindowHost.StartMain<CalculatorWindow>();
-            Calculator.Clear();
-        }
-
-        [TestMethod]
-        public void CanAdd2Numbers()
-        {
-            //Arrange
-            WindowHost
-                .StartMain<CalculatorWindow>()
-                .Clear()
-                .SelectNumber(1)
-                .SelectOperator(Operator.Addition)
-                .SelectNumber(1);
-
-            Calculator.PerformOperation();
-
-            //Act
-            Assert.AreEqual("2", Calculator.Result);
-        }
 
         [ClassInitialize]
         public static void RunBeforeAllTests(TestContext testContext)
